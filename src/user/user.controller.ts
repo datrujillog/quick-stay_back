f3fd46310c86aa98c9b866b51dff812b5a1cc560
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-10-01 07:43:11 -0500

    Refactorizar método findOne para utilizar el parámetro 'userId' en lugar de 'id'

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,7 +13,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -32,11 +32,11 @@ export class UserController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.remove(id);
   }
 }
