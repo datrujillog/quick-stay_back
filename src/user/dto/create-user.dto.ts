5046be6f75f0f1f0d560e8acd187f4363aa4ba91
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-10-01 06:27:57 -0500

    Agregar validaciones de clase a CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,39 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8, {
+    message:
+      'La contraseña es demasiado corta. Debe tener al menos 8 caracteres.',
+  })
+  @MaxLength(20, {
+    message:
+      'La contraseña es demasiado larga. Debe tener como máximo 20 caracteres.',
+  })
+  password: string;
+
+  //   @IsString()
+  //   role: string;
+
+  //   @IsString()
+  //   status: string;
+
+  //   created_at: Date;
+
+  //   updated_at: Date;
+
+  //   deleted_at: Date;
+
+  //   deleted?: boolean;
+
+  //   token: string;
+
+  //   token_exp: Date;
+}
