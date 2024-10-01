878398490f36b0d9341cfb9e1e40dfd21c98c046
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-10-01 05:20:23 -0500

    Actualizar dependencias de @nestjs/mongoose y mongoose a las versiones 10.0.10 y 8.7.0 respectivamente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/quickstay-DB'),
+
+    UserModule,
   ],
+
   // controllers: [AppController],
   // providers: [AppService],
 })
