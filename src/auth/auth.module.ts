776928ad7ccc942716dcc9a3a9c6eefa8f3cb1f5
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-10-01 20:14:15 -0500

    Refactorizar módulo de autenticación para agregar dependencias de JWT y Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Auth, AuthSchema } from './entities/auth.entity';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema }]),
+    PassportModule,
+    JwtModule.register({
+      secret: 'yourSecretKey',  // Pon una clave secreta segura aquí
+      signOptions: { expiresIn: '60m' },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule { }
